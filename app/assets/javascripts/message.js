488e614c663fdc960d859d2e793ca1e48fa67d4b
ronjeff0816 <ronjeffersonlotino@gmail.com>
2020-05-06 15:52:44 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -89,7 +89,7 @@ $(function(){
   };
 
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-    setInterval(reloadMessages, 7000);
+    setInterval(reloadMessages, 3000);
    }
 
  });
